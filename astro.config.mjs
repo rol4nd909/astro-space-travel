ff2f822f709af54f864697bd34d7f29a7eea3eab
Rol4nd <mail@rolandfranke.nl>
2024-01-27 15:59:41 +0100

    Added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rol4nd909.github.io',
-  integrations: [icon()]
+  integrations: [icon(), react()]
 });
