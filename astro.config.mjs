c4dc7bbc48a53250619f953509f16a02ae21c4f5
Rol4nd <mail@rolandfranke.nl>
2025-04-22 11:27:17 +0200

    Replace Icon component with SVG logo in Header and SimpleHeader components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import icon from "astro-icon";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: 'https://rol4nd909.github.io',
-  integrations: [
-    icon(),
-    react()
-  ]
+  integrations: []
 });
